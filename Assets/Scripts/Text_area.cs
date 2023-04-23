a6a2a30857361253a87c46145d7d74301f399e5d
niteesh <niteeshchandrav@gmail.com>
2023-04-23 01:15:20 -0700

    Button missing

--- Assets/Scripts/Text_area.cs
+++ Assets/Scripts/Text_area.cs
@@ -2,16 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using OpenAi;
+using UnityEngine.SceneManagement;
+
+
 
 public class Text_area : MonoBehaviour
 {
     public  TMP_Text tm;
     public TMP_Text tm2;
     public Button change;
- 
+    public Button chat_redirect;
+    public string second_scene;
+
     // for configuration of API keys
     public Configuration configuration;
 
@@ -26,6 +30,10 @@ public class Text_area : MonoBehaviour
         Configuration.GlobalConfig = OpenAiApi.ReadConfigFromUserDirectory();
         Button btn = change.GetComponent<Button>();
         btn.onClick.AddListener(change_text);
+
+
+        Button chat_change = chat_redirect.GetComponent<Button>();
+        chat_change.onClick.AddListener(Change_screen);
         tm.text = "20+10"; // Maths Questioning Area
         //Debug.Log(tm);
         tm2.text = "Answering Area"; // solutation area
@@ -43,5 +51,10 @@ public class Text_area : MonoBehaviour
         var completion = await gpt.CreateCompletion(prompt);
         Debug.Log(completion.Text);
         tm2.text = completion.Text;
+    }
+
+    void Change_screen()
+    {
+        SceneManager.LoadScene(second_scene);
     }
 }
